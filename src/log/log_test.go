59eb0aab0f609605eaa7f0f0b3bdec347528c470
Terry.Mao <83873308@qq.com>
2013-12-27 23:12:40 +0800

    add log module

--- src/log/log_test.go
+++ src/log/log_test.go
@@ -9,19 +9,19 @@ func TestLog(t *testing.T) {
 	logger, err := New("/tmp/test.log", Error)
 	if err != nil {
 		logger.Error("write to stdout")
-		t.Errorf("new a logger failed")
+		t.Error("new a logger failed")
 	}
 
 	logger.Error("come on %d", 111)
 	logger.Info("won't in log")
 	if err = logger.Close(); err != nil {
-		t.Errorf("close a logger failed")
+		t.Error("close a logger failed")
 	}
 
 	logger, err = New("", Error)
 	if err != nil {
 		logger.Error("write to stdout")
-		t.Errorf("new a logger failed")
+		t.Error("new a logger failed")
 	}
 
 	logger.Error("write to stdout, %s", "test")
