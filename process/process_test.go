69248d5b7dcfe7264ef61519f9ec4cd998ef1d7e
Terry.Mao <83873308@qq.com>
2014-03-19 14:18:22 +0800

    add more comment

--- process/process_test.go
+++ process/process_test.go
@@ -21,6 +21,7 @@ import (
 )
 
 // mkdir -p /tmp/test && chown -R nobody:nobody /tmp/test
+// sudo go test
 func TestInit(t *testing.T) {
 	if err := Init("nobody nobody", "./", "/tmp/test/process_test.pid"); err != nil {
 		t.Error(err)
