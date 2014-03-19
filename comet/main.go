960281c3b4d4a76c56608ff2bf811c8600572991
Terry.Mao <83873308@qq.com>
2014-03-19 15:16:28 +0800

    fix "os.Exit won't execute defer" bug

--- comet/main.go
+++ comet/main.go
@@ -21,7 +21,6 @@ import (
 	"github.com/Terry-Mao/gopush-cluster/log"
 	"github.com/Terry-Mao/gopush-cluster/perf"
 	"github.com/Terry-Mao/gopush-cluster/process"
-	"os"
 	"runtime"
 	"time"
 )
@@ -39,14 +38,14 @@ func main() {
 	Conf, err = InitConfig(ConfFile)
 	if err != nil {
 		Log.Error("NewConfig(\"%s\") error(%v)", ConfFile, err)
-		os.Exit(-1)
+		return
 	}
 	// Set max routine
 	runtime.GOMAXPROCS(Conf.MaxProc)
 	// init log
 	if Log, err = log.New(Conf.LogFile, Conf.LogLevel); err != nil {
 		Log.Error("log.New(\"%s\", %s) error(%v)", Conf.LogFile, Conf.LogLevel, err)
-		os.Exit(-1)
+		return
 	}
 	// if process exit, close log
 	defer Log.Close()
@@ -69,7 +68,7 @@ func main() {
 	zk, err := InitZookeeper()
 	if err != nil {
 		Log.Error("InitZookeeper() error(%v)", err)
-		os.Exit(-1)
+		return
 	}
 	// if process exit, close zk
 	defer zk.Close()
@@ -78,7 +77,7 @@ func main() {
 	time.Sleep(time.Second)
 	if err = process.Init(Conf.User, Conf.Dir, Conf.PidFile); err != nil {
 		Log.Error("process.Init() error(%v)", err)
-		os.Exit(-1)
+		return
 	}
 	// init signals, block wait signals
 	HandleSignal(signalCH)
