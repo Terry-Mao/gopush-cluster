8611b8e361a83da0ad2cbda68e6cec68669ce46b
Terry.Mao <83873308@qq.com>
2014-03-27 17:01:46 +0800

    add dependencies shell

---
+++ dependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Dependencies
+
+# go get -u github.com/Terry-Mao/gopush-cluster
+# go get -u github.com/Terry-Mao/goconf
+# go get -u github.com/garyburd/redigo/redis
+# go get -u code.google.com/p/go.net/websocket
+# go get -u github.com/samuel/go-zookeeper
+
+go get -u ./message/...
+go get -u ./comet/...
+go get -u ./web/...
